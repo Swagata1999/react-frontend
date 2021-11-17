b178bc8e3abd2c418be2a70d23ef0e379f843d04
Swagata1999 <swagatadas1999@gmail.com>
2021-11-17 13:38:18 +0530

    Commit

---
+++ src/FooterComponent.js/FooterComponent.js.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './FooterComponent.js.css';
+
+export default class FooterComponent.js extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+			<div className="FooterComponent.js">
+			</div>
+		)
+	}
+}
---
+++ src/ListUserComponent/ListUserComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './ListUserComponent.css';
+
+export default class ListUserComponent extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+			<div className="ListUserComponent">
+			</div>
+		)
+	}
+}
